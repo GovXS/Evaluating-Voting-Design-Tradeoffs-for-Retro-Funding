e82074c2840fb0599d8cc6e5ea71ea2db40bae26
EyalBriman <110922459+EyalBriman@users.noreply.github.com>
2024-07-08 10:47:01 +0300

    Update aggregation_methods.py

--- aggregation_methods.py
+++ aggregation_methods.py
@@ -23,11 +23,6 @@ def find_t_star(votes):
 #############################################################
 
 # Eyal's Input:
-#To my understanding the quadratic voting is not supposed to effect the aggrigation only the cost it self (bigger votes will be more expensive than smaller ones)
-# This is why Quadratic should just be a parameter in the simulation
-# I write some implementation here
-
-from scipy.optimize import brentq
 def f_k(t, k, n):
     if 0 <= t <= k / (n + 1):
         return 0
@@ -36,93 +31,40 @@ def f_k(t, k, n):
     elif (k + 1) / (n + 1) <= t <= 1:
         return 1
 
-def compute_median_with_moving_phantoms(F, profiles):
-    n = len(F) - 1  # number of functions in F, assuming indexing from 0 to n
-    m = len(profiles)  # number of profiles
+def compute_median_with_moving_phantoms(votes):
+    n, m = votes.shape  
     def median_with_phantoms(t_star):
-        median_values = [np.median([F[k](t_star) for k in range(n + 1)] + [profiles[i][j] for i in range(m)]) for j in range(m)]
+        F = [lambda t, k=k: f_k(t, k, n) for k in range(n + 1)]
+        median_values = [
+            np.median([F[k](t_star) for k in range(n + 1)] + [votes[i][j] for i in range(n)])
+            for j in range(m)
+        ]
         return np.median(median_values)
 
-    def find_t_star(): #this is simple binary search, we can use somthing better i guess-This can be changed to find somthing quicker That I add at the end of the script 
+    def find_t_star():
         low, high = 0.0, 1.0
-        while high - low > 1e-6:
+        epsilon = 0
+        while high - low > epsilon:
             mid = (low + high) / 2
             if np.sum([median_with_phantoms(mid) for _ in range(m)]) > 1:
                 high = mid
             else:
                 low = mid
         return low
-    
-    # Use Brent's method to find the root (t_star)
+
     t_star = find_t_star()
-    
-    return median_with_phantoms(t_star)
+    best_distribution = [median_with_phantoms(t_star) for _ in range(m)]
+    return best_distribution
 
-def create_phantom_system(n):
-    return [lambda t: f_k(t, k, n) for k in range(n + 1)]
-
-# Example usage:
-# Define the phantom system F with piecewise functions as described
-votes = np.array([
-    [0.2, 0.3, 0.5],
-    [0.3, 0.1, 0.6],
-    [0.1, 0.5, 0.4],
-    [0.0, 0.15, 0.85]
-])
-F = create_phantom_system(3)
-medians = []
-for j in range(votes.shape[1]):
-    profiles = votes[:, j]
-    median_value = compute_median_with_moving_phantoms(F, profiles)
-    medians.append(median_value)
-
-print("Computed Medians for each Project:", medians)
-
-#this is trivial but I guess should appear here also 
-def compute_mean_voting(profiles):
+def compute_mean_voting(votes):
     return np.mean(profiles, axis=0)
-
-### This is probably better for finding t*
-def median_with_phantoms(t_star):
-        median_values = [
-            np.median([F[k](t_star) for k in range(n + 1)] + [profiles[i][j] for i in range(m)]) 
-            for j in range(m)
-        ]
-        return np.median(median_values)
-def objective(t_star):# ti
-        return np.sum([median_with_phantoms(t_star) for _ in range(m)]) - 1
-t_star = brentq(objective, 0.0, 1.0)
-
-
-## we can also use gurobi for finding the optimal t, I have it installed on my computer but I am waiting for it to come back from the repair, It should be something like this
-
-import gurobipy as gp
-from gurobipy import GRB
-
-def find_optimal_t_star(n, m, F, p_hat):
-    model = gp.Model("Optimal_t_star")
-    t_star = model.addVar(vtype=GRB.CONTINUOUS, lb=0.0, ub=1.0, name="t_star")
-    total_median_expr = gp.LinExpr()
-    for j in range(m):
-        # Create F_t_star array (assuming F_t_star is defined somewhere in your context)
-        F_t_star = [F[k](t_star) for k in range(n + 1)]
         
-        values_for_median = F_t_star + [p_hat[i][j] for i in range(n)]
-        sorted_values = sorted(values_for_median)
-        n_values = len(sorted_values)
-        median_index = n_values // 2   
-        if n_values % 2 == 1:
-            median_expr_j = sorted_values[median_index]
-        else:
-            median_expr_j = (sorted_values[median_index - 1] + sorted_values[median_index]) / 2
-        
-        total_median_expr.add(median_expr_j)
-    model.addConstr(total_median_expr == 1, name="total_median_constraint")
-    model.optimize()
+def midpoint_rule(votes):
+    n, m = votes.shape
+    social_welfare = np.zeros(n)
+    for i in range(n):
+        social_welfare[i] = np.sum(np.sum(np.abs(votes - votes[i]), axis=1))
+    best_vote_index = np.argmin(social_welfare)
+    return votes[best_vote_index]
 
-    if model.status == GRB.OPTIMAL:
-        optimal_t_star = t_star.x
-        return optimal_t_star
-    else:
-        return None
 
