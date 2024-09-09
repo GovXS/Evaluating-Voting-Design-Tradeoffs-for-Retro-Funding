fb3e8c09a9b3d6fefa68c4cd53f7c4880d22d117
idrees535 <idrees535@gmail.com>
2024-09-09 21:56:42 +0500

    defined experiments

--- experiments/control.py
+++ experiments/control.py
@@ -4,7 +4,6 @@ import pandas as pd
 import os
 import sys
 from datetime import datetime
-timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 current_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.join(current_dir, '..')  # Adjust this to point to the correct folder
 sys.path.append(project_root)
@@ -39,44 +38,48 @@ print(allocation_df.head(10))
 
 
 # %%
-min_increase = 0.01
-max_increase = 10
-iterations = 10
-counter=1
+
+# Parameters for control sweep
+min_increase = 1
+max_increase = 30
+iterations = 100
 
 # Create a DataFrame to store the results of each iteration
-bribery_results = pd.DataFrame()
+control_results = pd.DataFrame()
 
-# Generate 100 values of desired_increase_percentage from 0.01 to 10
-desired_increase_percentages = np.linspace(min_increase, max_increase, iterations)
+# Generate 30 values of desired_increase from 1 to 30
+desired_increase_values = np.linspace(min_increase, max_increase, iterations)
 
-# Iterate through each desired_increase_percentage
-for i, desired_increase_percentage in enumerate(desired_increase_percentages, 1):
-    print(f"Iteration {i}/{iterations} with desired_increase_percentage: {desired_increase_percentage}")
+# Iterate through each desired_increase value
+for i, desired_increase in enumerate(desired_increase_values, 1):
+    print(f"Iteration {i}/{iterations} with desired_increase: {desired_increase}")
 
-    # Evaluate bribery costs for the current desired increase percentage
-    bribery_results_df = eval_metrics.evaluate_bribery_optimized(num_rounds, desired_increase_percentage)
+    # Evaluate control results for the current desired increase
+    control_results_constant_desired_increase_df = eval_metrics.evaluate_control(num_rounds, desired_increase)
 
-    # Calculate the average bribery cost for each voting rule over all rounds
-    avg_bribery_costs = bribery_results_df.mean()
+    # Calculate the average control results over all rounds
+    avg_control_results = control_results_constant_desired_increase_df.mean()
 
-    # Convert the result to a DataFrame and add the desired_increase_percentage column
-    avg_bribery_costs_df = avg_bribery_costs.to_frame().T
-    avg_bribery_costs_df['desired_increase_percentage'] = desired_increase_percentage
+    # Log both the percentage and absolute amount of the desired increase
+    avg_control_results['desired_increase'] = desired_increase
+
+    # Convert the Series to a DataFrame for concatenation
+    avg_control_results_df = avg_control_results.to_frame().T
 
     # Append the results to the DataFrame using pd.concat
-    bribery_results = pd.concat([bribery_results, avg_bribery_costs_df], ignore_index=True)
+    control_results = pd.concat([control_results, avg_control_results_df], ignore_index=True)
 
 # Display the results after the loop
-print(bribery_results)
-output_path=os.path.join(output_dir, 'bribery_experiment_results_{timestamp}.csv')
+print(control_results)
 
-bribery_results.to_csv(output_path, index=False)
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+# Save the results to a CSV file
+control_results.to_csv(os.path.join(output_dir, 'control_experiment_results_{timestamp}.csv'), index=False)
 
 
 # %%
-bribery_results.head(100)
-print("Bribery experiment Completed")
-print("Results saved to {output_path}")
+print(control_results.head(100))
+
 
 
---
+++ experiments/robustness.py
@@ -0,0 +1,45 @@
+# Import libraries
+import numpy as np
+import pandas as pd
+import os
+import sys
+from datetime import datetime
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.join(current_dir, '..')  # Adjust this to point to the correct folder
+sys.path.append(project_root)
+from model.VotingModel import VotingModel
+from model.EvalMetrics import EvalMetrics
+
+# Add the directory containing the VotingModel to the Python path
+
+from model.VotingRules import VotingRules
+
+# Initialize simulation parameters
+num_voters = 144
+num_projects = 600
+total_op_tokens = 30e6
+num_rounds = 100
+voter_type = 'mallows_model'
+quorum=17
+# Initialize the model
+model = VotingModel(voter_type=voter_type, num_voters=num_voters, num_projects=num_projects, total_op_tokens=total_op_tokens)
+
+# Initialize the evaluation metrics
+model.step()
+eval_metrics = EvalMetrics(model)
+current_dir = os.path.dirname(os.path.abspath(__file__))  # Get the current file's directory
+output_dir = os.path.join(current_dir, '..', 'data', 'vm_data')  # Define relative path
+
+allocation_df=model.compile_fund_allocations()
+allocation_df.to_csv(os.path.join(output_dir, 'allocation_df.csv'), index=False)
+print("Fund Allocations for each project")
+print(allocation_df.head(10))
+
+
+robustness_results = eval_metrics.evaluate_robustness(num_rounds)
+robustness_results.to_csv(os.path.join(output_dir,'robustness_results_{timestamp}.csv'), index=False)
+print("Robustness Results:")
+print(robustness_results.head(100))
+
+print("Experiment Completed")
