9754636a1151494e754db5e70d4e05833054e5d7
idrees535 <idrees535@gmail.com>
2024-10-16 01:19:56 +0500

    mallows model reults included

--- experiments/experiments_config.py
+++ experiments/experiments_config.py
@@ -1,11 +1,11 @@
 # config.py
-num_voters = 108
-num_projects = 230
-total_op_tokens = 10e6
-num_rounds = 2
-voter_type = 'r4_voting_matrix'
+num_voters = 40
+num_projects = 63
+total_op_tokens = 8e6
+num_rounds = 100
+voter_type = 'mallows_model'
 quorum = 17
 min_increase = 1
-max_increase = 100
-iterations = 10
-experiment_description = 'r4-voting-matrix with 10e6 funds'
+max_increase = 30
+iterations = 30
+experiment_description = f'mallows_model_{num_voters}_{num_projects}_{total_op_tokens}'
